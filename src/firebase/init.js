61609bde25b5701d648029f8c1cdd381beadb952
sajuryy <sajuryy@gmail.com>
2019-03-16 04:22:35 +0300

    init

---
+++ src/firebase/init.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAKAfBRQWrYQEOv_c5QCTMM7Jx2G0JaItc",
+    authDomain: "vue-chat-690c8.firebaseapp.com",
+    databaseURL: "https://vue-chat-690c8.firebaseio.com",
+    projectId: "vue-chat-690c8",
+    storageBucket: "vue-chat-690c8.appspot.com",
+    messagingSenderId: "417319006989"
+  };
+  const firebaseApp = firebase.initializeApp(config);
+  firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+  export default firebaseApp.firestore();
